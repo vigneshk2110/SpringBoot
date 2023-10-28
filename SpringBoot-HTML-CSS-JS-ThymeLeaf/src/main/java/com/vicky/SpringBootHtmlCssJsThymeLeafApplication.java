ee784546408825d2c1e41482bb5fa90066487475
VigneshK <104199131+vigneshk2110@users.noreply.github.com>
2023-10-28 07:49:44 +0530

    SpringBoot-HTML-CSS-JS-ThymeLeaf

---
+++ SpringBoot-HTML-CSS-JS-ThymeLeaf/src/main/java/com/vicky/SpringBootHtmlCssJsThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.vicky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHtmlCssJsThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHtmlCssJsThymeLeafApplication.class, args);
+	}
+
+}
